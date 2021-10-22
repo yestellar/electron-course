f9f64bb3868df8bbd13e894a3e28cc03e1287549
Dmitriy Smirnov <vonrimsd17@gmail.com>
2021-10-22 09:20:44 +0200

    6 - Bitcoin price widget

--- index.js
+++ index.js
@@ -1,55 +1,35 @@
 const { app, BrowserWindow } = require('electron')
 
-// console.log('userDataPath: ', app.getPath('userData'))
+let widgetWindow = null
 
-let mainWindow = null 
-let modalWindow = null
-
-function createMainWindow() {
-    mainWindow = new BrowserWindow({
-       width: 800,
-       height: 600,
-       show: false
+function createWidgetWindow() {
+    widgetWindow = new BrowserWindow({
+        width: 250,
+        height: 88,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        },
+        frame: false,
+        // titleBarStyle: 'hidden',
+        // titleBarOverlay: true,
+        // backgroundColor: '#444',
+        alwaysOnTop: true,
+        resizable: false,
+        hasShadow: false,
+        // thickFrame: false,
+        transparent: true
     })
-    mainWindow.loadFile('index.html')
-    mainWindow.on('closed', () => {
-        mainWindow = null
-        debugger
+    
+    widgetWindow.loadFile('index.html')
+    
+    widgetWindow.on('closed', () => {
+        widgetWindow = null
     })
-    mainWindow.once('ready-to-show', () => {
-        mainWindow.show()
-    })
-    // mainWindow.webContents.openDevTools()
 }
 
-function createModalWindow() {
-    modalWindow = new BrowserWindow({
-        width: 400,
-        height: 300,
-        show: false,
-        parent: mainWindow,
-        modal: true
-    })
-    modalWindow.loadFile('modal.html')
-    modalWindow.on('closed', () => {
-        modalWindow = null
-        debugger
-    })
-    modalWindow.once('ready-to-show', () => {
-        modalWindow.show()
-    })
-    // mainWindow.webContents.openDevTools()
-}
+app.on('ready', createWidgetWindow)
 
-app.on('ready', () => {
-    createMainWindow()
-    createModalWindow()
-})
-
-app.on('window-all-closed', () => {  
+app.on('window-all-closed', () => {
     app.quit()
-})
-
-app.on('before-quit', (event) => {
-    console.log('before-quit');
 })
