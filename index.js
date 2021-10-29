11353ad26d65677ca11f845a574a4961cfe14b74
Dmitriy Smirnov <vonrimsd17@gmail.com>
2021-10-29 17:57:15 +0200

    8 - IPC + Bitcoin widget refactoring

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const { app, BrowserWindow, shell, webContents } = require('electron')
+const { app, BrowserWindow, shell, ipcMain } = require('electron')
+const coingecko = require('./coingecko')
 
 let widgetWindow = null
 
 function createWidgetWindow() {
     widgetWindow = new BrowserWindow({
         width: 250,
-        height: 88,
+        // height: 88,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false
@@ -33,8 +34,15 @@ function createWidgetWindow() {
         return { action: 'deny' }
     })
 
-    console.log( webContents.getAllWebContents() )
+    // widgetWindow.webContents.openDevTools()
 }
+
+ipcMain.on('data-request', async (e, ...args) => {
+    const coins = await coingecko.fetchCoins('bitcoin,ethereum,litecoin,uniswap,dogecoin')
+
+    e.reply('data-response', coins)
+    // widgetWindow.webContents.send('data-response', coinNames)
+})
 
 app.on('ready', createWidgetWindow)
 
